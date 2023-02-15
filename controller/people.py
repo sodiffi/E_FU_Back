a4e24f13bd846142af299255fe7e7643ffe5a7c6
HuiiKuo <11136008@ntub.edu.tw>
2023-02-15 11:21:01 +0900

    新增 - 新增復健者、復健者查詢

---
+++ controller/people.py
@@ -0,0 +1,69 @@
+from flask import Blueprint, request, Response
+from model import peopleModel
+import json
+import uuid
+# from coder import MyEncoder
+from flask import app
+from model.db import mongo
+from datetime import datetime
+
+from .util import checkParm, ret
+
+peopleProfile = Blueprint("people", __name__, url_prefix="/people")
+
+@peopleProfile.route("/show", methods=["GET"])
+def get():
+    data = peopleModel.getpeople()
+    print((data))
+    print(type(data))
+    result = {"success": False, "data": data}
+    return ret(result)
+
+
+
+# @peopleProfile.route("/add", methods=["POST"])
+# def add(name,gender,birth,height,weight):
+#     data = {"name":name,"gender":gender,"birth":birth,"height":height,"weight":weight}
+#     print((data))
+#     result = {"success": False, "data": data}
+#     peopleModel.addpeople(data)
+
+@peopleProfile.route("/add", methods=["POST"])
+def add():
+    content = request.json
+    print(content)
+    uid = uuid.uuid4()
+    uid=str(uid)
+    print(uid)
+    name = content["name"]
+    gender = content["gender"]
+    birth = content["birth"]
+    # birth = datetime.strptime(content["birth"],'%Y-%m-%d').date()
+    height = content["height"]
+    weight = content["weight"]
+    data = peopleModel.addpeople(uid,name, gender, birth, height, weight)
+    print((data))
+    result = {"success": False, "data": data}
+    return ret(result)
+
+
+@peopleProfile.route("/search", methods=["POST"])
+def findname():
+    content = request.json
+    print(content)
+    name = content["name"]
+    data = peopleModel.findname(name)
+    print((data))
+    result = {"success": False, "data": data}
+    return ret(result)
+
+
+@peopleProfile.route("/edit", methods=["POST"])
+def edit():
+    content = request.json
+    print(content)
+    uid = content["uid"]
+    data = peopleModel.finduid(uid)
+    print((data))
+    result = {"success": False, "data": data}
+    return ret(result)
