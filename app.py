37f2caec460887b70abef586b0fef0f0730e9dfa
sodiffi <10656010@ntub.edu.tw>
2023-03-08 11:21:41 +0800

    新增 - 基本紀錄新增

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 # app.py
 import os
-from flask import Flask, Response, request, abort, redirect
+from flask import Flask, Response, request, abort, redirect,jsonify
 import requests
+from itsdangerous import TimedJSONWebSignatureSerializer as TJSS
 
 import json
 import sys
-from controller import(user)
+from controller import(user,record)
 from model.db import mongo
+from controller.util import checkParm, ret
 
 from model import userModel
 
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ABCDEFhijklm'
 app.config["MONGO_URI"] = "mongodb+srv://numbone112:i3PO8xrZj1KRwz83@cluster0.5rqnhen.mongodb.net/efu"
 mongo.init_app(app) # initialize here!
 print(type(mongo))
 print((mongo.db.name))
 app.register_blueprint(user.userProfile)
-
+app.register_blueprint(record.recordAPI)
 
 
 @app.route('/', methods=["POST"])
---
+++ model/recordModel.py
@@ -0,0 +1,13 @@
+import json
+from model.util import group
+from model.db import mongo
+
+def record(target):
+    to_save=[]
+    raw=json.loads(target['raw'])
+   
+    for b in raw:
+   
+        b['arrange_id']=target['arrange_id']
+        to_save.append(b)
+    return mongo.db.record.insert_many(to_save)
