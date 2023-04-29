77530a218893d43942d892d4568503d306632104
HuiiKuo <11136008@ntub.edu.tw>
2023-04-29 17:06:22 +0800

    新增 - 查詢復健師、查詢復健師班表、查詢預約、新增預約

--- controller/people.py
+++ controller/people.py
@@ -7,7 +7,7 @@ from flask import app
 from model.db import mongo
 from datetime import datetime
 
-from .util import checkParm, ret
+from .util import checkParm, ret,quickRet
 
 peopleProfile = Blueprint("people", __name__, url_prefix="/people")
 
@@ -87,4 +87,68 @@ def editpeople():
         print((data))
         result["mes"] = "編輯成功"
         result["success"] = True
+    return ret(result)
+
+# @peopleProfile.route("/arrange", methods=["POST"])
+# def arrange():
+#     content = request.json
+#     print(content)
+#     uid = uuid.uuid4()
+#     arrange_id=str(uid)
+#     e_id = content["e_id"]
+#     project_id = content["project_id"]
+#     need_times = content["need_times"]
+#     finish_group = content["finish_group"]
+#     recovery_time = content["recovery_time"]
+#     order = content["order"]
+#     account = content["account"]
+#     access = peopleModel.finduid(e_id)
+#     print(access)
+#     data = peopleModel.arrange(e_id, project_id, need_times, finish_group, recovery_time,order,account)
+#     print((data))
+#     result = {"success": False, "data": data}
+#     return ret(result)
+
+
+
+@peopleProfile.route("/tfind", methods=["GET"])
+def findt():
+    data = peopleModel.findTherapist()
+    print((data))
+    result = {"success": False, "data": data}
+    return ret(result)
+
+
+@peopleProfile.route("/twork/<t_id>", methods=["GET"])
+def findtwork(t_id):
+    data = peopleModel.findTherapist(t_id)
+    result = {"success": False, "data": data}
+    return ret(result)
+
+@peopleProfile.route("/appointment/<t_id>", methods=["POST"])
+def appointment(t_id):
+    cond = ["id","start_date", "time","item","p_id","done","remark"]
+    check = checkParm(cond, request.json)
+    if type(check) == dict:
+        temp=peopleModel.appointment(
+                t_id,
+                check["id"],
+                check["start_date"],
+                check["time"],
+                check["item"],
+                check["p_id"],
+                check["done"],
+                check["remark"]
+            )
+        print(temp.inserted_id)
+        return quickRet("Appointment successful")
+    else : 
+        return quickRet(check)
+
+@peopleProfile.route("/appointment/find", methods=["POST"])
+def findappointment():
+    content = request.json
+    p_id = content["p_id"]
+    data = peopleModel.findappointment(p_id)
+    result = {"success": False, "data": data}
     return ret(result)
--- model/peopleModel.py
+++ model/peopleModel.py
@@ -1,6 +1,7 @@
 import json
 from model.util import group
 from model.db import mongo
+from datetime import datetime,timedelta
 
 # name,gender,birth,height,weight
 
@@ -18,3 +19,17 @@ def finduid(uid):
 
 def editpeople(uid,name,gender,birth,height,weight,disease_id):
     return mongo.db.people.update_one({"uuid":uid},{"$set": { "name":name,"gender":gender,"birth":birth,"height":height,"weight":weight,"disease_id":disease_id }})
+
+def findTherapist():
+    return list(mongo.db.user.find({"permission":2},{"_id":0}))
+
+def findTherapistWork(t_id):
+    return list(mongo.db.work.find({"t_id":t_id},{"_id":0}))
+
+def appointment(t_id,id,start_date,time,item,p_id,done,remark):
+    start_date +="  00:00:00"
+    datetime_object = datetime.strptime(f'{start_date}', '%Y-%m-%d %H:%M:%S')
+    return mongo.db.appointment.insert_one({"t_id":t_id,"id":id,"start_date":datetime_object,"time":time,"item":item,"p_id":p_id,"done":done,"remark":remark})
+
+def findappointment(p_id):
+    return list(mongo.db.appointment.find({"p_id":p_id},{"_id":0}))
