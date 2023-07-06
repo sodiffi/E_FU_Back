8066c6333834c14d9a0f71ef0b403652ad66d8b1
HuiiKuo <11136008@ntub.edu.tw>
2023-07-06 23:21:12 +0800

    修改 - 查詢mo伴

--- controller/mo.py
+++ controller/mo.py
@@ -1,18 +1,20 @@
-from flask import Blueprint, request
+from flask import Blueprint, request,Response
 from model import moModel
-from .util import checkParm, ret
+from .util import checkParm, ret, quickRet
 from model.db import mongo
 
 moProfile = Blueprint("mo", __name__, url_prefix="/mo")
 
-@moProfile.route("/mo/<user_id>", methods=["GET"])
+@moProfile.route("/<user_id>", methods=["GET"])
 def get(user_id):
+    # print(user_id)
     mo_list = list(mongo.db.user.find({"id": user_id}, {"_id": 0, "friend": 1}))
     friend_ids = []
     for i in mo_list:
         friend_ids.append(i["friend"])
+    print(friend_ids)
     try:
-        data = moModel.getmofriend(friend_ids)
+        data = moModel.getmofriend(friend_ids[0])
         result = {"success": False, "data": data}
         return ret(result)
     except:
