0dab968408c85ea6ca6762a0326bc2ee61e8912b
HuiiKuo <11136008@ntub.edu.tw>
2023-07-09 14:35:05 +0800

    修改 - 新增邀約

--- model/inviteModel.py
+++ model/inviteModel.py
@@ -3,13 +3,22 @@ from model.util import group
 from model.db import mongo
 from datetime import datetime,timedelta
 
-def addinviteid(data):
-    #return mongo.db.inviteid.insert_one(data)
-    try:
-        mongo.db.inviteid.insert_one(data)
-        return ""
-    except:
-        return "error"
+def addinvite(id,name,m_id,friend,time,remark):
+    return mongo.db.Invite.insert_one(
+        {
+            "id": id,
+            "name": name,
+            "m_id": m_id,
+            "friend": friend,
+            "time": time,
+            "remark": remark,
+        }
+    )
+    # try:
+    #     mongo.db.inviteid.insert_one(data)
+    #     return ""
+    # except:
+    #     return "error"
 
 """ def get():
     return mongo.db.user.insert_one(inviteid) """
