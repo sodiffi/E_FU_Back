b1a63fc9ec221de1657cc395776fa0d94609f8f7
HuiiKuo <11136008@ntub.edu.tw>
2023-07-20 21:52:24 +0800

    新增 - 查詢邀約列表

--- model/inviteModel.py
+++ model/inviteModel.py
@@ -3,7 +3,7 @@ from model.util import group
 from model.db import mongo
 from datetime import datetime,timedelta
 
-def addinvite(id,name,m_id,friend,time,remark): #新增活動
+def addinvite(id,name,m_id,friend,time,remark): #新增邀約
     return mongo.db.Invite.insert_one(
         {
             "id": id,
@@ -16,8 +16,7 @@ def addinvite(id,name,m_id,friend,time,remark): #新增活動
     )
 
 
-
-def editinvite(id,name,m_id,friend,time,remark): #修改活動
+def editinvite(id,name,m_id,friend,time,remark): #修改邀約
     return mongo.db.Invite.update_one(
         {"id": id, "m_id":m_id},
         {
@@ -30,20 +29,52 @@ def editinvite(id,name,m_id,friend,time,remark): #修改活動
         },
     )
 
+def getacceptList(m_id):
+    return list(mongo.db.Invite_detail.aggregate(
+        [
+            {
+                '$match': {
+                    'user_id': m_id, 
+                    'accept': True
+                }
+            }, {
+                '$project': {
+                    'id': '$a_id', '_id': 0
+                }
+            }
+        ]
+    ))
 
-def findmid(m_id, id):
-    return list(mongo.db.Invite.find({"m_id": m_id,"id": id}, {"_id": 0}))
-def getinviteList(id,name,m_id,friend,time,remark): #新增活動
-    return mongo.db.Invite.insert_one(
-        {
-            "id": id,
-            "name": name,
-            "m_id": m_id,
-            "friend": friend,
-            "time": time,
-            "remark": remark,
-        }
-    )
+
+def getinviteList(m_id,acceptList): #邀約列表
+    return list(mongo.db.Invite.aggregate(
+            [
+                {
+                    '$match': {
+                        'm_id': m_id
+                    }
+                }, {
+                    '$project': {
+                        'id': 1, 'name': 1, 'time': 1, '_id': 0
+                    }
+                }, {
+                    '$unionWith': {
+                        'coll': 'Invite', 
+                        'pipeline': [
+                            {
+                                '$match': {
+                                    'id': {'$in': acceptList}
+                                }
+                            }, {
+                                '$project': {
+                                    'id': 1, 'name': 1, 'time': 1, '_id': 0
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        ))
 
 
 def getinviteDetail(id,name,m_id,friend,time,remark): #修改活動
