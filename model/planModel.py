6f46cfc8a84a1cbe15a607e97867c9338ae960fd
sodiffi2 <11136005@ntub.edu.tw>
2023-09-15 15:52:05 +0800

    新增 - 查詢運動計畫

--- model/planModel.py
+++ model/planModel.py
@@ -6,11 +6,12 @@ import numpy as np
 
 
 def addPlan(plan):  
+    ##新增時需判斷是否有重複區間
     return mongo.db.plan.insert_one(plan)
 
 
 def getPlan(user_id):
-    return list(mongo.db.plan.find({"id": user_id}, {"_id": 0}))[0]
+    return list(mongo.db.plan.find({"user_id": user_id}, {"_id": 0}))
 
 
 def editPlan(plan, user_id):
