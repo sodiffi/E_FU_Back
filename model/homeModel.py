c37a3bdaec1eb13adc266e3024da550892f9b96f
sodiffi2 <11136005@ntub.edu.tw>
2023-10-24 21:52:17 +0800

    修改 - 當週運動查詢

--- model/homeModel.py
+++ model/homeModel.py
@@ -10,32 +10,39 @@ def getHome(user_id):
     #計算三項平均
     sportsList = list(mongo.db.Invite_detail.find({"user_id": user_id}))
 
+    score = [0, 0, 0]
+    count = [0, 0, 0]
     for i in sportsList:
-        score = [0, 0, 0, 0]
-        count = [0, 0, 0, 0]
+        
         for d in i["done"]:
-            score[d["type_id"]] = score[d["type_id"]] + d["level"]
-            count[d["type_id"]] = count[d["type_id"]] + 1
-    for i in range(len(score)):
-        score[i] = score[i] / count[i]
+            print(d)
+            print(type(d))
+            score[d["type_id"]] = score[d["type_id"]-1] + int(d["level"])
+            count[d["type_id"]] = count[d["type_id"]-1] + 1
+    #     print(score,count)
+    print(score,count)
+    for i in range(1,len(score)):
+        if(count[i]!=0):score[i] = score[i] / count[i]
 
-    #取得當週運動紀錄(mongodb 需撰寫invite list view table)
+    #取得當週運動紀錄
     firstDayOfWeek = 1
     today = datetime.now()
-    currentDayOfWeek = today.date()
+    currentDayOfWeek = today.weekday()
 
     diffToFirstDay = currentDayOfWeek - firstDayOfWeek
     if diffToFirstDay < 0:
         diffToFirstDay += 7
     start_of_week = today - timedelta(days=diffToFirstDay)
-    end_of_week = start_of_week + timedelta(day=7)
-
+    end_of_week = start_of_week + timedelta(days=7)
+    #當週運動情形
     sportsday = list(
-        mongo.db.Invite_detail.events.find(
+        mongo.db.invite_list.find(
             {"time": {"$gte": start_of_week, "$lt": end_of_week}}
         )
     )
-    weekdays = list(mongo.db.plan.find({"user_id": user_id}))[0]
+    #有哪些天要運動
+    weekdays = list(mongo.db.plan.find({"user_id": user_id}))[0]["execute"]
+    print(weekdays)
     done_plan_list = []
     for i in range(7):
         # 0 無計畫也無運動
@@ -52,6 +59,6 @@ def getHome(user_id):
 
         done_plan_list.append(done_plan)
     
-    #運動日程(待補，可與當週運動紀錄合併，查詢invite list之後，篩選當週之後的資料，當週給當週運動，未來的資料給運動日程)
-    
+    #運動日程(待補，查詢invite list之後，篩選未來的資料五筆給運動日程)
+
     return {"avg_score": score,"done_plan":done_plan_list}
