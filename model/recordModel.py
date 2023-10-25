01d835ef626e94b6d7ad96561ab353ac484cafa1
HuiiKuo <11136008@ntub.edu.tw>
2023-10-25 08:33:39 +0800

    修改-新增紀錄

--- model/recordModel.py
+++ model/recordModel.py
@@ -3,12 +3,21 @@ from model.util import group
 from model.db import mongo
 
 
-def record(i_id,done,rawdata):
+def record(detail,rawdata):
     return {
-        "appointment": mongo.db.appointment.update_one(
-            {"id": i_id}, {"$set": {"done": done}}
-        ),
-        "raw": mongo.db.rehabilion.insert_many(rawdata),
+        "Invite_detail":mongo.db.Invite_detail.update_one(
+            {
+                'user_id': detail["user_id"],
+                'i_id': detail["i_id"]
+            },
+            {
+                '$set': {
+                    'done': detail["done"],
+                    'score': detail["score"]
+                }
+            }
+        )
+        # "raw": mongo.db.record.insert_many(rawdata)
     }
 
 # def record(a_id,done,target):
