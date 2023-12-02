a21fe4e0b7988ac3d03932329afa0400191f375a
HuiiKuo <11136008@ntub.edu.tw>
2023-12-02 12:47:24 +0800

    新增 - 運動次數長條圖

--- model/planModel.py
+++ model/planModel.py
@@ -38,5 +38,50 @@ def editPlan(plan, user_id):
     else:
         return "無法新增"
 
-
 # def delPlan(plan_id)
+
+def barChart(user_id):
+    detail = list(mongo.db.Invite_detail.aggregate([
+            { "$match": { "user_id": user_id ,"accept":1, "total_score":{"$exists":True}} },
+            { "$group": { "_id": None, "id": { "$addToSet": "$i_id" } } },
+            { "$project": { "_id": 0, "id": 1 } }
+        ]))[0]["id"]
+
+    twelve_months_ago = datetime.now() - timedelta(days=365)
+
+    return list(mongo.db.Invite.aggregate([
+        {
+            "$match":{
+                "id":{"$in":detail},
+                "time": {
+                    "$gte": twelve_months_ago,
+                    "$lt": datetime.now()
+                }
+            }
+        }, {
+            '$project': {
+                'month': {
+                    '$month': {
+                        '$toDate': '$time'
+                    }
+                }, 
+                '_id': 0, 
+                'id': 1
+            }
+        }, {
+            '$group': {
+                '_id': '$month', 
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$addFields': {
+                'month': '$_id'
+            }
+        }, {
+            '$project': {
+                '_id': 0
+            }
+        }
+    ]))
